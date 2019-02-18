e6cf43a8322a01a8c0052aad4d1c9b9b63f7a412
Karan Rampal <karan.rampal@ericsson.com>
2019-02-18 16:22:39 +0100

    Re-factor code for clarity

--- main.py
+++ main.py
@@ -2,22 +2,49 @@
 """Test mysql"""
 
 import argparse
+import sys
+import getpass
 import mysql.connector as mysql
+import pandas as pd
 
 def args_parse():
     """Parse command line arguments
     """
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Save mysql database as csv file")
+    parser.add_argument('-u',
+                        '--username',
+                        default="root",
+                        help="Username to connect to the database server")
+    parser.add_argument('-n',
+                        '--hostname',
+                        default="localhost",
+                        help="Server location")
     parser.add_argument('-d',
-                        '--data_dir',
-                        default=None,
+                        '--data_path',
+                        default="ml_mimo",
                         help="Directory containing the database")
     return parser.parse_args()
 
 def main():
     """Main function
     """
-    pass
+    args = args_parse()
+
+    try:
+        password = getpass.getpass()
+    except ValueError as error:
+        sys.exit('Error: {0}'.format(error))
+
+    cnx = mysql.connect(user=args.username,
+                        password=password,
+                        host=args.hostname,
+                        database=args.data_path)
+
+    events_df = pd.read_sql("SELECT * FROM events", cnx)
+    print(events_df.head())
+
+    cnx.close()
+
 
 if __name__ == "__main__":
     main()
